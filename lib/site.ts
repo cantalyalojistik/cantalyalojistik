f683746859cb52b166aff5ff76f11eebba76ff7d
Cantalya Lojistik <cantalyanakliyat@users.noreply.github.com>
2026-08-31 01:18:48 +0300

    cantalyalojistik.com yayin ayarlarini ekle

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 export const siteUrl =
   process.env.NEXT_PUBLIC_SITE_URL ??
-  'https://4vksvg4q9x-afk.github.io/cantalyalojistik';
+  'https://cantalyalojistik.com';
 
 export const siteBasePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 
